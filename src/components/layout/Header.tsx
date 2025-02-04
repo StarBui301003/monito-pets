6b8ef2f942c7fed762d393c7250f0c6e4c1552ae
StarBui301003 <saobc3010@gmail.com>
2025-02-04 18:30:50 +0700

    done navbar desktop

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,6 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
